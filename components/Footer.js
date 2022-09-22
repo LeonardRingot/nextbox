aaa90671e3f6d97c49d1fb6c9911ff0f0185b410
LeonardRingot <leonardringot6@gmail.com>
2022-09-22 11:44:09 +0200

    UpdateNextBoxLeonardRingot

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
+
 import React from "react";
 
 export default function Footer() {
@@ -11,14 +11,14 @@ export default function Footer() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+         
           <span>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>{" "}
           par Léonard Ringot
         </a>
         
-        <Link href="about"> about </Link>
+        
       </footer>
     </>
   );
