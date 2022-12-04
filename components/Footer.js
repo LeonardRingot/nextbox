246750d1901c891d9760f565b73aa6d5c8a0c2ea
LeonardRingot <leonardringot6@gmail.com>
2022-12-04 19:07:07 +0100

    add some css

--- components/Footer.js
+++ components/Footer.js
@@ -1,25 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Style from '../styles/Footer.module.css' 
 
 export default function Footer() {
   return (
     <>
+    <div className={Style.myFooter}>
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/LeonardRingot/nextbox"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
           <span>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>{" "}
-          par Léonard Ringot UPDATE REPRISE
+          Brief Nextbox réalisé par Léonard Ringot  Concepteur Développeur d'Applications - Simplon
         </a>
         
-        <Link href="about"> about </Link>
+        
       </footer>
+      </div>
     </>
   );
 }
---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import Style from '../styles/Header.module.css' 
+// import { BsLinkedin } from "react-icons/bs";
+export default function Header(){
+    
+   
+    return(
+        <>
+        <div className={Style.myHeader} >
+        <h1>Bienvenue sur mon NextBox - Léonard RINGOT</h1>
+        {/* //<BsLinkedin></BsLinkedin> */}
+        </div>
+        </>
+        
+    )
+}
